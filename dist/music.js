8f91082e91e7bf619f28cf8f37f9a799154e537c
e <876027276@qq.com>
2020-04-05 23:57:13 +0800

    Site updated: 2020-04-05 23:57:12

---
+++ dist/music.js
@@ -0,0 +1,37 @@
+const ap = new APlayer({
+    container: document.getElementById('aplayer'),
+    fixed: true,
+    autoplay: false,
+    audio: [
+      {
+        name: "烟火里的尘埃",
+        artist: '华晨宇',
+        url: 'http://m10.music.126.net/20200406000421/ce89954428d97f077ef6a9efaadeff24/ymusic/5d63/5150/0851/5f226aac018cafc2cb248f7d28fbd5b4.mp3',
+        cover: 'http://p1.music.126.net/_49Xz_x9kTTdEgmYYk6w2w==/6672936069046297.jpg?param=300x300',
+      },
+      {
+        name: 'Sold Out',
+        artist: 'Hawk Nelson',
+        url: 'http://m10.music.126.net/20200406000732/c1d48917ee1481220e3c6678b70fbb80/ymusic/550b/540b/550e/bac6e33322ca318d3ff2bd6535bd59cd.mp3',
+        cover: 'http://p2.music.126.net/UR8jAfqus_o1j_QkaYkZ_g==/109951163664241365.jpg?param=300x300',
+      },
+      {
+        name: '麻雀',
+        artist: '李荣浩',
+        url: 'http://m10.music.126.net/20200406001413/a8d31b49dd092f24789f3658b9778880/ymusic/555b/0f58/0609/b1e0b087cb826dde13b21cbaa504f963.mp3',
+        cover: 'http://p2.music.126.net/TzlSVBiNtpRD2b7MT2Hi-w==/109951164527590793.jpg?param=300x300',
+      },
+	  {
+        name: '斗牛',
+        artist: '华晨宇',
+        url: 'http://m10.music.126.net/20200406001606/78ca30f9f96e55cc2fe3fcaaabbb0b78/ymusic/0253/555b/5609/5928c52d80b10dfab06aacd42733a738.mp3',
+        cover: 'http://p1.music.126.net/OBY8dfbP-Q002e4OECrqJA==/109951164703135281.jpg?param=300x300',
+      },
+      {
+        name: '庐州月',
+        artist: '许嵩',
+        url: 'http://m10.music.126.net/20200406001743/c9bee90009da3857ffaf9667119ce21a/ymusic/86de/4d7b/c0dc/b45d9ddf9d7837f0d0bb71707ee1decf.mp3',
+        cover: 'http://p1.music.126.net/3hqcQrXZ39kDCCzV7QbZjA==/34084860473122.jpg?param=300x300',
+      }
+    ]
+});
